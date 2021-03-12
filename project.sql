4cc3008c0eb8e8fa771f2ee80bdf1651d3b4db98
Nuclear Catapult <nuclear.catapult@gmail.com>
2021-03-12 16:37:13 -0600

    added session table

--- project.sql
+++ project.sql
@@ -1,14 +1,24 @@
-DROP TABLE IF EXISTS student;
-/* or we can replace the above line by adding 'IF NOT EXISTS' to the
-user creation statement */
-
 /* we use the name 'web_user' because 'user' is not a valid name in
 Oracle Database */
 CREATE TABLE web_user (
-    username VARCHAR(20),
+    id VARCHAR(20),
     /* size of 255 in case we use a password hash */
     password VARCHAR(255), 
+    /*
+    Oracle Database does not support ENUM. We need to find an alternative.
     type ENUM('student', 'administrator')
+    */
+    PRIMARY KEY (id)
 );
 
-INSERT INTO web_user VALUES ('root', 'toor', 'administrator');
+create table web_user_session (
+    id varchar2(32),
+    web_user_id varchar2(8),
+    session_date date,
+    PRIMARY KEY (id),
+    FOREIGN KEY (web_user_id)
+        REFERENCES web_user(id)
+        ON DELETE CASCADE
+);
+
+INSERT INTO web_user VALUES ('root', 'toor'/*, 'administrator'*/);
