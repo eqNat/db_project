e9c7cd6a1af571d4170128709b1699027587f765
Nuclear Catapult <nuclear.catapult@gmail.com>
2021-03-17 21:56:53 -0500

    student can now view grades

--- welcomepage.php
+++ welcomepage.php
@@ -8,18 +8,32 @@ if ($sessionid == "") {
 } 
 else {
   // here we can generate the content of the welcome page
-  echo("Hello, welcome to my Website.<br/>
+
+  // https://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php 
+  // I implemented endsWith()
+  $from = "login.html";
+  if (substr(getenv("HTTP_REFERER"), -strlen($from)) === $from) {
+    echo("Hello, welcome to my Website.<br/>");
+  }
+
+  echo("
   Click here to <a href='logout_action.php?sessionid=$sessionid' tite='Logout'>Logout.</a>
   ");
 
   echo("<br/>$isstudent");
   echo("<br/>$isadmin");
 
-  if($isadmin == 1){
-    echo("
+  if ($isadmin == 1){
+    echo("<BR>
       Click here to <a href='user_management.php?sessionid=$sessionid' tite='Logout'>do admin stuff.</a>
     ");
   }
+  if ($isstudent == 1) {
+    echo ("<BR>
+      <a href='table.html?sessionid=$sessionid' tite='Logout'>View Grades</a>
+        ");
+  }
+
   echo("<BR>
     <a href='change_password.html'>Change Password</a>
   ");
